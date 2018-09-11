d1624ac31fd62821be1e1f56a53fbd252d0fae5d
renxj <renxiaojuan@suiyueyule.com>
2018-09-11 10:41:18 +0800

    入门

---
+++ app.js
@@ -0,0 +1,78 @@
+// 导入koa，和koa 1.x不同，在koa2中，我们导入的是一个class，因此用大写的Koa表示:
+const Koa = require('koa');
+
+const bodyParser = require('koa-bodyparser');
+
+const controller = require('./controller');
+
+// 创建一个Koa对象表示web app本身:
+const app = new Koa();
+
+// 对于任何请求，app将调用该异步函数处理请求：
+// app.use(async (ctx, next) => {
+//     await next();
+//     ctx.response.type = 'text/html';
+//     ctx.response.body = '<h1>Hello, koa2!</h1>';
+// });
+
+// log request URL:
+app.use(async (ctx, next) => {
+    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+    await next();
+});
+
+// parse request body:
+app.use(bodyParser());
+
+// add controllers:
+app.use(controller());
+
+
+// 在端口3000监听:
+app.listen(3000);
+console.log('app started at port 3000...');
+
+
+const nunjucks = require('nunjucks');
+
+function createEnv(path, opts) {
+    var
+        autoescape = opts.autoescape === undefined ? true : opts.autoescape,
+        noCache = opts.noCache || false,
+        watch = opts.watch || false,
+        throwOnUndefined = opts.throwOnUndefined || false,
+        env = new nunjucks.Environment(
+            new nunjucks.FileSystemLoader('views', {
+                noCache: noCache,
+                watch: watch,
+            }), {
+                autoescape: autoescape,
+                throwOnUndefined: throwOnUndefined
+            });
+    if (opts.filters) {
+        for (var f in opts.filters) {
+            env.addFilter(f, opts.filters[f]);
+        }
+    }
+    return env;
+}
+
+var env = createEnv('views', {
+    watch: true,
+    filters: {
+        hex: function (n) {
+            return '0x' + n.toString(16);
+        }
+    }
+});
+
+// var s = env.render('hello.html', { name: '小明' });
+// console.log(s);
+
+// var s = env.render('hello.html', { name: '<script>alert("小明")</script>' });
+// console.log(s);
+
+console.log(env.render('hello.html', {
+    header: 'Hello',
+    body: 'bla bla bla...'
+}));
